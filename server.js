753b5ff7e814c1a028808535ed341f9e1478ff5e
Olga-Olga <olga_l@ukr.net>
2023-10-22 20:57:12 +0300

    commit

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 import app from "./app.js";
 // import mongoose from "module";
 const { PORT = 3000 } = process.env;
-app.listen(PORT, () => {
-  console.log("Server running. Use our API on port:", PORT);
-});
+// app.listen(PORT, () => {
+//   console.log("Server running. Use our API on port:", PORT);
+// });
 
-// const DB_HOST =
-//   "mongodb+srv://olga_olga:<password>@cluster0.plzu3r6.mongodb.net/contacts?retryWrites=true&w=majority";
+const DB_HOST =
+  "mongodb+srv://olga_olga:<password>@cluster0.plzu3r6.mongodb.net/contacts?retryWrites=true&w=majority";
 
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => {
-//     app1.listen(PORT, () => {
-//       console.log("Server running. Use our API on port: 3000");
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//     process.exit(1);
-//   });
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app1.listen(PORT, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
